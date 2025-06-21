b7d2c4a74114428aa395d288cd98ae2549bf8c96
lam <lambro2510@gmail.com>
2025-06-21 18:10:14 +0700

    update project

--- src/main/java/vn/devTool/core/base/type/BusinessErrorCode.java
+++ src/main/java/vn/devTool/core/base/type/BusinessErrorCode.java
@@ -5,11 +5,15 @@ import org.springframework.http.HttpStatus;
 
 /**
  * Danh sách các mã lỗi chung dùng cho toàn hệ thống.
+ * 0-50 cho validate - check tồn tại
+ * 51-100 cho lỗi phát sinh trong hệ thống
+ * 100-1xx cho lỗi phát sinh ngoài hệ thống
  */
 @Getter
 public enum BusinessErrorCode {
 
     // Common error code
+    DATA_NOT_FOUND("COMMON_001", "Data not found", HttpStatus.BAD_REQUEST),
     BAD_REQUEST("COMMON_400", "Bad request", HttpStatus.BAD_REQUEST),
     UNAUTHORIZED("COMMON_401", "Unauthorized", HttpStatus.UNAUTHORIZED),
     FORBIDDEN("COMMON_403", "Forbidden", HttpStatus.FORBIDDEN),
@@ -23,15 +27,26 @@ public enum BusinessErrorCode {
 
     // Account service
     ACCOUNT_NOT_FOUND("ACCOUNT_01", "Invalid username or password", HttpStatus.BAD_REQUEST),
-    KEYCLOAK_SERVER_ERROR("ACCOUNT_02", "Authentication error", HttpStatus.BAD_REQUEST),
+
+    KEYCLOAK_SERVER_ERROR("ACCOUNT_100", "Authentication error", HttpStatus.BAD_REQUEST),
 
     // ==== UPLOAD MCRS ERROR ====
     UPLOAD_STORAGE_NOT_FOUND("UPLOAD_STORAGE_01", "Upload storage not found", HttpStatus.BAD_REQUEST),
     REQUIRED_FIELD_NOT_NULL("UPLOAD_STORAGE_02", "Both storage type and storage id is null", HttpStatus.BAD_REQUEST),
     UPLOAD_FAIL("UPLOAD_STORAGE_03", "Upload file to storage is error", HttpStatus.INTERNAL_SERVER_ERROR),
+    UPLOAD_FILE_NOT_FOUND("UPLOAD_STORAGE_4", "Upload file not found", HttpStatus.BAD_REQUEST),
 
-    UPLOAD_FILE_NOT_FOUND("UPLOAD_STORAGE_10", "Upload file not found", HttpStatus.BAD_REQUEST),
-    DOWNLOAD_FILE_ERROR("UPLOAD_STORAGE_11", "Download file error", HttpStatus.INTERNAL_SERVER_ERROR),
+    DOWNLOAD_FILE_ERROR("UPLOAD_STORAGE_100", "Download file error", HttpStatus.INTERNAL_SERVER_ERROR),
+
+    // ==== AI GENERATE MCRS ERROR ====
+    AI_MODEL_NOT_FOUND("AI_MODEL_01", "Ai model not found", HttpStatus.BAD_REQUEST),
+    AI_MODEL_INVALID_URL("AI_MODEL_02", "Ai model not found url", HttpStatus.BAD_REQUEST),
+    AI_MODEL_MISSING_API_KEY("AI_MODEL_03", "Ai model missing api key", HttpStatus.BAD_REQUEST),
+
+
+    AI_MODEL_REQUEST_FAILED("AI_MODEL_100", "Ai model request fail", HttpStatus.BAD_REQUEST),
+
+    PROMPT_SETTING_NOT_FOUND("PROMPT_SETTING_01", "Prompt setting not found", HttpStatus.BAD_REQUEST),
 
     ;
 
--- src/main/java/vn/devTool/core/utils/StringUtils.java
+++ src/main/java/vn/devTool/core/utils/StringUtils.java
@@ -8,4 +8,8 @@ public class StringUtils {
   public static String extractCurrencyPair(String symbol) {
     return symbol.replaceAll("/", "");
   }
+
+  public static boolean isBlank(String url) {
+    return org.apache.commons.lang3.StringUtils.isBlank(url);
+  }
 }
