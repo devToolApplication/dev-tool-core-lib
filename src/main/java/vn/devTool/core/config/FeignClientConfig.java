6a8b13498e1b5d00bc323681112691bd17f7673d
lam <lambro2510@gmail.com>
2025-07-27 13:57:15 +0700

    update

--- src/main/java/vn/devTool/core/config/FeignClientConfig.java
+++ src/main/java/vn/devTool/core/config/FeignClientConfig.java
@@ -1,8 +1,6 @@
 package vn.devTool.core.config;
 
-import feign.RequestInterceptor;
-import feign.Response;
-import feign.codec.ErrorDecoder;
+import feign.Contract;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,7 +12,6 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @Configuration
-@ConfigurationProperties(prefix = "feign.client.config.default")
 @Log4j2
 public class FeignClientConfig {
 
